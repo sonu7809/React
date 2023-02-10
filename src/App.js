44f755b630b2ac6628bd994c2e670bf1a1e01828
sonu7809 <soumendrabehera2@gmail.com>
2023-02-10 12:27:34 +0530

    react

---
+++ src/App.js
@@ -0,0 +1,19 @@
+// import logo from './logo.svg';
+import './App.css';
+
+let name ="somu"
+function App() {
+  return (
+   <>
+   <nav>
+    <li>Home</li>
+    <li>About</li>
+    <li>Contact</li>
+   </nav>
+   <h1>Hello {name}</h1>
+   
+   </>
+  );
+}
+
+export default App;
